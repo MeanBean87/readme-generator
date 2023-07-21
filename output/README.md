03395ff10c3bed660bc7f34b03c3b119392244d2
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-20 21:22:47 -0400

    Updated readme.md output.

---
+++ output/README.md
@@ -0,0 +1,33 @@
+# test
+        
+## Description
+test
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Table of Contents    
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+test
+
+## Usage
+test   
+
+## Contributing
+test 
+
+## Tests
+test
+
+## License
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license
+
+## Questions
+Feel free to reach out to me with any questions!
+* test: [Github](https://github.com/test)
+* Email: test
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,11 +2,11 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "MIT") {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    return `## License\n[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
   } else if (license === "Apache") {
-    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    return `## License\n[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
   } else if (license === "GPL") {
-    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    return `## License\n[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
   } else if (license === "None") {
     return ``;
   }
